7018b5ffcbb3ea610a9287bc53cf0318f502ca2a
Mcagdas <mcagdasbalci@gmail.com>
2024-05-20 10:34:01 +0300

    all project

---
+++ src/app/shared/models/authReq.ts
@@ -0,0 +1,14 @@
+export interface emailAuthReq {
+  email: string;
+  password: string;
+  authenticatorCode?: string;
+}
+export interface emailAuthRes {
+  accessToken: tokenInfo;
+  requiredAuthenticatorType: number;
+}
+
+export interface tokenInfo {
+  token: string;
+  expirationDate: string;
+}
